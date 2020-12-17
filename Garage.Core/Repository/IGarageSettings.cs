5a42bc780a865e6766e283e4f20131e44a1b2d1e
nick <yannick.katambo@outlook.com>
2020-12-17 13:30:12 +0200

    initial commit with core class added

---
+++ Garage.Core/Repository/IGarageSettings.cs
@@ -0,0 +1,19 @@
+﻿using Garage.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garage.Core.Repository
+{
+    public interface IGarageSettings
+    {
+        #region // Manage asset category
+        void AddCategory(Adm_AssetCategory category);
+        void UpdateCategory(Adm_AssetCategory category);
+        void DeleteCategory(Adm_AssetCategory category);
+        List<Adm_AssetCategory> GetAllCategory();
+        Adm_AssetCategory GetCategoryById(int id);
+
+        #endregion
+    }
+}
