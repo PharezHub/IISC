3427e4e9dd61798d15893d81ec490050d1f1dcfb
NickKatambo <yannick.katambo@outlook.com>
2020-12-23 23:56:12 +0200

    Progress commit on edit trigger

--- Garage.Web/Pages/Routines/EditTrigger.cshtml.cs
+++ Garage.Web/Pages/Routines/EditTrigger.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage.Core.Models;
+using Garage.Core.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,34 @@ namespace Garage.Web.Pages.Routines
 {
     public class EditTriggerModel : PageModel
     {
-        public void OnGet()
+        private readonly IRoutineRepository routineRepository;
+
+        public EditTriggerModel(IRoutineRepository routineRepository)
         {
+            this.routineRepository = routineRepository;
+        }
+
+        [BindProperty]
+        public Adm_TriggerType TriggerType { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            TriggerType = routineRepository.GetTriggerById(id);
+
+            if (TriggerType == null)
+            {
+                return RedirectToPage("NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return Page();
         }
     }
 }
