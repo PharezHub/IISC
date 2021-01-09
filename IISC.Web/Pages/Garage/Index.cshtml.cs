ad6a1627c4b7b0fca082d16920b93374a5cc93cb
NickKatambo <yannick.katambo@outlook.com>
2021-01-09 23:55:44 +0200

    progress commit

--- IISC.Web/Pages/Garage/Index.cshtml.cs
+++ IISC.Web/Pages/Garage/Index.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage.Core.Models;
+using Garage.Core.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,19 @@ namespace IISC.Web.Pages.Garage
 {
     public class IndexModel : PageModel
     {
+        private readonly INavigationRepository navigationRepository;
+
+        public IndexModel(INavigationRepository navigationRepository)
+        {
+            this.navigationRepository = navigationRepository;
+        }
+
+        [BindProperty]
+        public IEnumerable<Gen_SystemArea> SystemAreaList { get; set; }
+
         public void OnGet()
         {
+            SystemAreaList = navigationRepository.GetGarageSystemArea();
         }
     }
 }
