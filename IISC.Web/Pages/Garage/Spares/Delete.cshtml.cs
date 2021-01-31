432f37386c8d086d6d424b65b57d1d30786bb977
NickKatambo <yannick.katambo@outlook.com>
2021-02-01 00:55:27 +0200

    Done with Parts catalog.

--- IISC.Web/Pages/Garage/Spares/Delete.cshtml.cs
+++ IISC.Web/Pages/Garage/Spares/Delete.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Garage.Core.Models;
+using Garage.Core.Repository;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,29 @@ namespace IISC.Web.Pages.Garage.Spares
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IAssetRepository assetRepository;
+
+        public DeleteModel(IAssetRepository assetRepository)
         {
+            this.assetRepository = assetRepository;
+        }
+
+        [BindProperty]
+        public AdmPartsCatalog PartsCatalog { get; set; }
+
+        public IActionResult OnGet(int id)
+        {
+            PartsCatalog = assetRepository.GetPartsCatalogById(id);
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (PartsCatalog.ID > 1)
+            {
+                assetRepository.DeletePartCatalog(PartsCatalog.ID);
+            }
+            return RedirectToPage("/Garage/Spares/SparesList");
         }
     }
 }
