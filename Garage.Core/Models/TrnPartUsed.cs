f51f452c6f4737bdc8ab337e7083aa2156940af4
NickKatambo <yannick.katambo@outlook.com>
2021-02-27 16:55:24 +0200

    progress commit

--- Garage.Core/Models/TrnPartUsed.cs
+++ Garage.Core/Models/TrnPartUsed.cs
@@ -7,6 +7,7 @@ namespace Garage.Core.Models
 {
     public class TrnPartUsed
     {
+        [Key]
         public int ID { get; set; }
         public int MainID { get; set; }
 
@@ -17,6 +18,8 @@ namespace Garage.Core.Models
         public string DocketNo { get; set; }
         public int PartID { get; set; }
         public int Qty { get; set; }
+        public float? PartCost { get; set; }
+        public string PurchaseOrder { get; set; }
         public DateTime DateLogged { get; set; }
         public string LoggedBy { get; set; }
     }
