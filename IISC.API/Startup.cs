ff52f985666aa4aa21636c14a6984aeb3692f83b
NickKatambo <yannick.katambo@outlook.com>
2021-04-12 09:05:41 +0200

    Progress commit - service registration done.

--- IISC.API/Startup.cs
+++ IISC.API/Startup.cs
@@ -1,7 +1,11 @@
+using API.Core.APIContext;
+using IISC.API.Repository;
+using IISC.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +30,9 @@ namespace IISC.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            services.AddScoped<IPurchaseOrderRepository, PurchaseOrderService>();
+            services.AddDbContextPool<APIDbContext>(options => options.UseSqlServer(Environment.GetEnvironmentVariable("ERPConn")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Scaw Integration API", Version = "v1" });
