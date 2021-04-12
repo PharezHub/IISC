3a6e6f27810af03d8055faa4d6db1168327c82a1
NickKatambo <yannick.katambo@outlook.com>
2021-04-12 07:47:46 +0200

    API and ERP integration.

---
+++ API.Core/Models/PoDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Core.Models
+{
+    public class PoDetail
+    {
+        public int ID { get; set; }
+        public string PurchaseOrder { get; set; }
+        public int LineId { get; set; }
+        public DateTime PoDate { get; set; }
+        public string PoDescription { get; set; }
+        public string Stockcode { get; set; }
+        public string StockDescription { get; set; }
+        public int StockQty { get; set; }
+        public float StockCost { get; set; }
+        public string SupplierCode { get; set; }
+        public string SupplierName { get; set; }
+
+    }
+}
